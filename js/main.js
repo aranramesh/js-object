60b5c59fd6d85ee859dc8aece1ade91d5c575052
aranramesh <svramesh692@gmail.com>
2022-08-30 12:41:02 +0530

    js objects

---
+++ js/main.js
@@ -0,0 +1,136 @@
+const person = { 
+    name: 'John',
+    age: 20,
+    native:"USA",
+    dob:"29-02-2002"
+ };
+ console.log(person); 
+ // 1
+ 
+ const laptop = {
+    brand :"lenovo",
+    model :"e41",
+    ram: "4gb"
+ };
+ console.log(laptop)
+ // 2
+
+ const car= {
+    brand :"lamborgini",
+    model :"urus",
+    fueltype:"diesel",
+    color:"orange"
+ };
+ console.log(car)
+ // 3
+
+ const mobile = {
+    brand :"vivo",
+    model :"u20",
+    ram: "4gb",
+    color:"blue",
+ 
+ };
+ console.log(mobile)
+ 
+ // 4
+ 
+ const comapny ={
+    cname : "zoho",
+    worker : "10000",
+    location:"tenkasi",
+    branch:"chennai"
+
+ };
+ console.log(company)
+
+ // 5
+ const student = {
+     name : "ajith",
+     age : 43,
+     area:"kovai"
+
+
+ };
+ console.log(student)
+
+ // 6
+ 
+  const aadhar = {
+     name:"kumar",
+     age : 55,
+     dob:30-01-1934
+
+  };
+  console.log("aadhar") 
+  
+ //  7
+
+ const watch={
+     brand:"apple",
+     color:"black",
+     rupees:30000,
+     seller :"apple",
+     releaseyear:"2022"
+ };
+ console.log(watch)
+
+ // 8
+
+
+ const books ={
+     author:"brain",
+     bname:"timemanagement",
+     rate:100,
+     lang:"english",
+     country:"india"
+ };
+ console.log(books)
+
+ // 9
+
+ const fabevy ={
+     ceo:"velmurugan",
+     location:"chennai",
+     branch:"pavoorchatram",
+     founddate:"2019",
+     newbranch:"chennai"
+
+
+ };
+ console.log(fabevy)
+ // 10
+
+ const notebook ={
+     brand:"classmate",
+     mrp:"50",
+     headoffice:"noida",
+     fyear:"1998",
+     awards:"superbrand",
+     
+
+
+ };
+ console.log(notebook)
+
+ // 11
+
+ const mail ={
+     brand:"gmail",
+     headcompany:"google",
+     usercount:"1.5 Billion",
+     service:"free",
+     ceo:"sundhar"
+
+ }; 
+ console.log(mail)
+ // 12
+const predator={
+    brand:"acer",
+    type:"gaming laptop",
+    ram:"16gb",
+    graphics:"rtx3060",
+    price:"60,000"
+};
+
+console.log(predator)

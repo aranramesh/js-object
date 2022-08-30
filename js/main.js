c39a34888e759b6073dd9955219ea5bd3d87223a
aranramesh <svramesh692@gmail.com>
2022-08-30 14:44:36 +0530

    js objects

--- js/main.js
+++ js/main.js
@@ -171,3 +171,162 @@ const hiring={
     company:"tcs"
 };
 console.log(hiring)
+
+// 16
+
+const monitor={
+    brand:"benq",
+    price:"11000",
+    screensz:"45ch",
+    type:"oled",
+    model:"a54"
+};
+console.log(monitor)
+
+// 17
+
+
+const train={
+    tname:"silambu express",
+    timing:"3.30pm",
+    spoint:"sencottah",
+    epoint:"egmore",
+    maxspeed:"120mph"
+};
+console.log(train)
+// 18
+
+const bus={
+    bname:"vrl",
+    owner:"aran buses",
+    location:"chennai",
+    headoffice:"tenkasi",
+    totalbuses:"58"
+
+};
+console.log(bus)
+// 19
+
+const game={
+    gname:"gta-v",
+    minram:"8gb",
+    graphicscard:"rtx1080",
+    processor:"i5",
+    size:"50gb"
+};
+console.log(game)
+// 20
+
+const country={
+    cname:"india",
+    population:"140crores",
+    capital:"delhi",
+    domesticproduct:"2.62 lakh crores()usd",
+    money:"Rupees"
+    
+};
+console.log(country)
+// 21
+
+
+const AmericanFootball={
+    Olympic: "No",
+    WorldGames: "Yes", 
+    country:"America",
+    contact:"full",
+    equipment:"football"
+};
+console.log(AmericanFootball)
+// 22
+
+const bike={
+    model:"ninja h2",
+    engine:"1000 CC",
+    fueltype:"petrol",
+    topspeed:"160",
+    owner:"ramesh"
+};
+console.log(bike)
+
+// 23
+
+const honda={
+    model:"civic",
+    nickname:"beautyqueen",
+    topspeed:"200-mph",
+    color:"white",
+    owner:honde
+};
+console.log(honda)
+
+// 24
+
+
+const chrome={
+    work:"browser",
+    owner:"google",
+    speciality:"fast browser",
+    size:"50mb",
+    award:"most used browser"
+};
+
+console.log(chrome)
+// 25
+
+const vscode={
+    use:"type-code",
+    benifit:"separate code",
+    owns:"microsoft",
+    special:"most used coder",
+    color:"blue"
+};
+console.log(vscode)
+// 26
+
+const windows={
+    version="windows 10",
+    keyprice:"199 usd",
+    owner:"microsoft",
+    relaeseyear:"2020",
+    benifit:"most advanced version"
+};
+console.log(windows)
+
+// 27
+
+const playstation={
+    varinat:"ps5",
+    version:"latest version",
+    price:"60000",
+    owns:"sony corparation",
+    country:"japan"
+};
+console.assertlog(playstation)
+
+// 28
+
+const pc={
+    model:"hulk",
+    processor:"i 9  10th generation",
+    graphics:"rtx3090",
+    price:"500000",
+    ram:"32gb"
+};
+console.log(pc)
+
+// 29
+
+const facebook={
+    owner:"mark zuckerberg",
+    mothercompany:"meta",
+    country:"America",
+    earning:"2.5billion",
+    speciality:"most used social media"
+    
+};
+console.log(facebook)
+
+// 30
+
+
+
